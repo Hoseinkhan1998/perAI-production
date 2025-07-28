52f8eed8f223967e40dfaae81d908bc9e7fe3603
Hoseinkhan1998 <hosein.mazinani.1998@gmail.com>
2025-07-29 00:14:44 +0330

    fix landing page

--- src/components/HomePage/HeaderSection.vue
+++ src/components/HomePage/HeaderSection.vue
@@ -10,7 +10,7 @@ function handleDisplay(item) {
 
 <template>
   <!-- عکس اصلی -->
-  <div class="flex justify-center pt-[20vh]  bg-cover bg-center" :style="{ backgroundImage: `url(/images/landing.png)`, height: '110vh' }">
+  <div class="flex justify-center pt-[100px]  bg-cover bg-center h-[700px]" :style="{ backgroundImage: `url(/images/landing.png)` }">
     <div class="text-neutral-100 flex flex-col gap-5">
       <p class="text-[67px] text-center leading-snug font-bold">
         because each<span class="text-[#2A58FB]"> project</span><br />
